be61198712e01d04c22df5ccfac08ed5ca5b966e
unaib-saiyad <unaibsaiyad18@gmail.com>
2024-03-09 11:00:26 +0530

    React Setup

---
+++ src/components/authentication/Signup.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../assets/styles/authentication/Signup.css';
+
+function Signup() {
+  return (
+    <div className='auth-bg'>Signup</div>
+  )
+}
+
+export default Signup;
